6ec92ade028afde8e211895843e5aeb30994b456
xzboss <942875315@qq.com>
2023-12-29 16:59:28 +0800

    fix: 🐛 ex1

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,99 +1,99 @@
-import { defineUserConfig } from "vuepress";
-import type { DefaultThemeOptions } from "vuepress";
-import recoTheme from "vuepress-theme-reco";
+import { defineUserConfig } from 'vuepress';
+import type { DefaultThemeOptions } from 'vuepress';
+import recoTheme from 'vuepress-theme-reco';
 
 export default defineUserConfig({
-  title: "vuepress-theme-reco",
-  description: "Just playing around",
+  title: 'vuepress-theme-reco',
+  description: 'Just playing around',
   theme: recoTheme({
-    style: "@vuepress-reco/style-default",
-    logo: "/logo.png",
-    author: "reco_luan",
-    authorAvatar: "/head.png",
-    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
-    docsBranch: "main",
-    docsDir: "example",
-    lastUpdatedText: "",
+    style: '@vuepress-reco/style-default',
+    logo: '/logo.png',
+    author: '942875315@qq.com',
+    authorAvatar: '/head.png',
+    docsRepo: 'https://github.com/vuepress-reco/vuepress-theme-reco-next',
+    docsBranch: 'main',
+    docsDir: 'example',
+    lastUpdatedText: '',
     // series 为原 sidebar
     series: {
-      "/docs/theme-reco/": [
+      '/docs/theme-reco/': [
         {
-          text: "module one",
-          children: ["home", "theme"],
+          text: 'module one',
+          children: ['home', 'theme']
         },
         {
-          text: "module two",
-          children: ["api", "plugin"],
-        },
-      ],
+          text: 'module two',
+          children: ['api', 'plugin']
+        }
+      ]
     },
     navbar: [
-      { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/reco/1/" },
-      { text: "Tags", link: "/tags/tag1/1/" },
-      { text: "Test", link: "/tags/tag1/1/" },
+      { text: 'Home', link: '/' },
+      { text: 'Categories', link: '/categories/reco/1/' },
+      { text: 'Tags', link: '/tags/tag1/1/' },
+      { text: 'Test', link: '/tags/tag1/1/' },
       {
-        text: "Docs",
+        text: 'Docs',
         children: [
-          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
-          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
-        ],
-      },
+          { text: 'vuepress-reco', link: '/docs/theme-reco/theme' },
+          { text: 'vuepress-theme-reco', link: '/blogs/other/guide' }
+        ]
+      }
     ],
     bulletin: {
       body: [
         {
-          type: "text",
+          type: 'text',
           content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-          style: "font-size: 12px;",
+          style: 'font-size: 12px;'
         },
         {
-          type: "hr",
+          type: 'hr'
         },
         {
-          type: "title",
-          content: "QQ 群",
+          type: 'title',
+          content: 'QQ 群'
         },
         {
-          type: "text",
+          type: 'text',
           content: `
           <ul>
             <li>QQ群1：1037296104</li>
             <li>QQ群2：1061561395</li>
             <li>QQ群3：962687802</li>
           </ul>`,
-          style: "font-size: 12px;",
+          style: 'font-size: 12px;'
         },
         {
-          type: "hr",
+          type: 'hr'
         },
         {
-          type: "title",
-          content: "GitHub",
+          type: 'title',
+          content: 'GitHub'
         },
         {
-          type: "text",
+          type: 'text',
           content: `
           <ul>
             <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
             <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
           </ul>`,
-          style: "font-size: 12px;",
+          style: 'font-size: 12px;'
         },
         {
-          type: "hr",
+          type: 'hr'
         },
         {
-          type: "buttongroup",
+          type: 'buttongroup',
           children: [
             {
-              text: "打赏",
-              link: "/docs/others/donate.html",
-            },
-          ],
-        },
-      ],
-    },
+              text: '打赏',
+              link: '/docs/others/donate.html'
+            }
+          ]
+        }
+      ]
+    }
     // commentConfig: {
     //   type: 'valie',
     //   // options 与 1.x 的 valineConfig 配置一致
@@ -107,6 +107,6 @@ export default defineUserConfig({
     //     // hideComments: true // 隐藏评论
     //   },
     // },
-  }),
+  })
   // debug: true,
 });
